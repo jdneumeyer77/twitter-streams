257c6a3e92e552a5fa882c9cb53260b5af9bf8b3
Jordan Neumeyer <jdneumeyer@gmail.com>
2016-11-16 21:45:19 -0700

    Swich over to streams.

--- src/main/scala/TwitterStreamer.scala
+++ src/main/scala/TwitterStreamer.scala
@@ -3,21 +3,27 @@
  * NOTE: this may stop working if at any point Twitter does some breaking changes to this API or the JSON structure.
  */
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpHeader.ParsingResult
 import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.hunorkovacs.koauth.domain.KoauthRequest
 import com.hunorkovacs.koauth.service.consumer.DefaultConsumerService
 import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 import org.json4s._
 import org.json4s.native.JsonMethods._
+
+import scala.collection.parallel.immutable
+import scala.concurrent.duration.Duration
 
 object TwitterStreamer extends App {
 
@@ -28,7 +34,7 @@ object TwitterStreamer extends App {
 	private val consumerSecret = "8H938MNsvMYHM79huL8l7rijNtSwczjLypubwzS8UKtp9KTtTL"
 	private val accessToken = "4447624812-5riaoBA6VGYXLUtJYSsIbnCGdsoVo3MwUi5aOHK"
 	private val accessTokenSecret = "42GTIaMCF5LqRJ7eDsrdyqutAlTELWJDcaxhfcxLiowKO"
-	private val url = "https://stream.twitter.com/1.1/statuses/filter.json"
+	private val url = "https://stream.twitter.com/1.1/statuses/sample.json"
 
 	implicit val system = ActorSystem()
 	implicit val materializer = ActorMaterializer()
@@ -36,65 +42,50 @@ object TwitterStreamer extends App {
 
 	private val consumer = new DefaultConsumerService(system.dispatcher)
 
-	//Filter tweets by a term "london"
-	val body = "track=london"
 	val source = Uri(url)
 
+	val headers = generateAuthHeader()
 
+	val httpRequest: HttpRequest = HttpRequest(
+		method = HttpMethods.GET,
+		uri = source,
+		headers = headers
+	)
 
-	//Create Oauth 1a header
-	val oauthHeader: Future[String] = consumer.createOauthenticatedRequest(
-		KoauthRequest(
-			method = "POST",
-			url = url,
-			authorizationHeader = None,
-			body = Some(body)
-		),
-		consumerKey,
-		consumerSecret,
-		accessToken,
-		accessTokenSecret
-	) map (_.header)
-	
-	oauthHeader.onComplete {
-		case Success(header) =>
-			val httpHeaders: List[HttpHeader] = List(
-				HttpHeader.parse("Authorization", header) match {
-					case ParsingResult.Ok(h, _) => Some(h)
-					case _ => None
-				},
-				HttpHeader.parse("Accept", "*/*") match {
-					case ParsingResult.Ok(h, _) => Some(h)
-					case _ => None
-				}
-			).flatten
-			val httpRequest: HttpRequest = HttpRequest(
-				method = HttpMethods.POST,
-				uri = source,
-				headers = httpHeaders,
-				entity = HttpEntity(contentType = ContentType(MediaTypes.`application/x-www-form-urlencoded`, HttpCharsets.`UTF-8`), string = body)
-			)
-			val request = Http().singleRequest(httpRequest)
-			request.flatMap { response =>
-				if (response.status.intValue() != 200) {
-					println(response.entity.dataBytes.runForeach(_.utf8String))
-					Future(Unit)
-				} else {
-					response.entity.dataBytes
-						.scan("")((acc, curr) => if (acc.contains("\r\n")) curr.utf8String else acc + curr.utf8String)
-						.filter(_.contains("\r\n"))
-						.map(json => Try(parse(json).extract[Tweet]))
-						.runForeach {
-							case Success(tweet) =>
-								println("-----")
-								println(tweet.text)
-							case Failure(e) =>
-								println("-----")
-								println(e.getStackTrace)
-						}
-				}
-			}
-		case Failure(failure) => println(failure.getMessage)
+	val connectionFlow = Http().outgoingConnectionHttps(source.authority.host.address, 443)
+
+	Source.single(httpRequest)
+				.via(connectionFlow)
+				.flatMapConcat {
+					case response if response.status.isSuccess() =>
+						response.entity.dataBytes
+					case other =>
+						println(s"Failed! $other")
+						Source.empty
+				}.filter(_.startsWith("\r\n")).map {
+					_.utf8String
+				}.runForeach(println).onComplete(_ => system.terminate)
+
+	def generateAuthHeader() = {
+		val oauthHeader: Future[String] = consumer.createOauthenticatedRequest(
+			KoauthRequest(
+				method = "GET",
+				url = url,
+				authorizationHeader = None,
+				body = None
+			),
+			consumerKey,
+			consumerSecret,
+			accessToken,
+			accessTokenSecret
+		).map(_.header)
+
+		// The program is isn't useful without this...
+		val authorizationHeader = Await.result(oauthHeader, Duration(5, TimeUnit.SECONDS))
+
+		List(HttpHeader.parse("Authorization", authorizationHeader) match {
+			case ParsingResult.Ok(h, _) => Some(h)
+			case _ => None
+		}).flatten
 	}
-
 }
