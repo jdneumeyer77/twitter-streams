18cc694a1648d287d17a96509b7d3f443e7d034e
Jordan Neumeyer <jdneumeyer@gmail.com>
2016-11-20 20:41:27 -0700

    Split out JsonProcessing into its own file. Lots of refactoring and cleanups.

---
+++ src/main/scala/JsonProcessing.scala
@@ -0,0 +1,71 @@
+import akka.NotUsed
+import akka.stream.FlowShape
+import akka.stream.scaladsl.{Balance, Flow, GraphDSL, Merge}
+import akka.util.ByteString
+import cats.data.Xor
+import de.knutwalker.akka.stream.JsonStreamParser
+import io.circe.{Decoder, Json, JsonObject}
+import GraphDSL.Implicits._
+
+sealed trait StreamedTweetType
+case class TweetEvent(json: Json) extends StreamedTweetType
+case class WarningEvent(json: Json) extends StreamedTweetType
+case class Event(json: Json) extends StreamedTweetType
+case class SingleFieldEvent(json: Json) extends StreamedTweetType
+object UnknownEvent extends StreamedTweetType
+
+object JsonProcessing extends JsonProcessing
+
+trait JsonProcessing {
+
+  // decode json to standard tweets.
+  def parallelTweetDecoder(parallel: Int)(implicit decoder: Decoder[Tweet]): Flow[StreamedTweetType, Tweet, NotUsed] =
+    Flow.fromGraph(GraphDSL.create() { implicit builder =>
+      val dispatcher = builder.add(Balance[StreamedTweetType](parallel))
+      val merged = builder.add(Merge[Tweet](parallel))
+
+      (0 until parallel).foreach { number =>
+        dispatcher.out(number) ~> decodeTweet.async ~> merged.in(number)
+      }
+
+      FlowShape(dispatcher.in, merged.out)
+    })
+
+  // parse and classify the json.
+  def parallelJsonClassifier(parallel: Int): Flow[ByteString, StreamedTweetType, NotUsed] = {
+    import io.circe.jawn.CirceSupportParser._
+    Flow.fromGraph(GraphDSL.create() { implicit builder =>
+      val dispatcher = builder.add(Balance[ByteString](parallel))
+      val merged = builder.add(Merge[StreamedTweetType](parallel))
+
+      (0 until parallel).foreach { number =>
+        dispatcher.out(number) ~> JsonStreamParser.flow[Json].async ~>
+          Flow[Json].map(tweetClassifier).async ~> merged.in(number)
+      }
+
+      FlowShape(dispatcher.in, merged.out)
+    })
+  }
+
+  private def decodeTweet(implicit decoder: Decoder[Tweet]): Flow[StreamedTweetType, Tweet, NotUsed] = {
+    Flow[StreamedTweetType].collect {
+      case TweetEvent(json) => decoder.decodeJson(json)
+    }.collect {
+      case Xor.Right(tweet) => tweet
+    }
+  }
+
+  private def tweetClassifier(json: Json): StreamedTweetType = {
+    val obj = json.asObject // top level object
+
+    if(obj.exists(jsonHasFields("warning"))) WarningEvent(json)
+    else if(obj.exists(jsonHasFields("event"))) Event(json)
+    else if(obj.exists(_.size == 1)) SingleFieldEvent(json)
+    else if(obj.exists(jsonHasFields("created_at", "text", "id_str"))) TweetEvent(json) // likely...
+    else UnknownEvent
+  }
+
+  def jsonHasFields(fields: String*)(json: JsonObject): Boolean = {
+    fields.forall(json.contains)
+  }
+}
--- src/main/scala/TwitterStreamer.scala
+++ src/main/scala/TwitterStreamer.scala
@@ -1,123 +1,129 @@
 /**
- * This is a fully working example of Twitter's Streaming API client.
- * NOTE: this may stop working if at any point Twitter does some breaking changes to this API or the JSON structure.
- */
+  * This is a fully working example of Twitter's Streaming API client.
+  * NOTE: this may stop working if at any point Twitter does some breaking changes to this API or the JSON structure.
+  */
 
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
 
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpHeader.ParsingResult
 import akka.http.scaladsl.model._
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Flow, Framing, Sink, Source}
+import akka.stream.scaladsl._
+import akka.stream.{ActorMaterializer, FlowShape}
 import akka.util.ByteString
 import cats.data.Xor
 import com.hunorkovacs.koauth.domain.KoauthRequest
 import com.hunorkovacs.koauth.service.consumer.DefaultConsumerService
 import com.typesafe.config.ConfigFactory
 import de.knutwalker.akka.stream.JsonStreamParser
+import io.circe._
+import io.circe.generic.semiauto._
+import io.circe.generic.auto._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
-import io.circe._
-import io.circe.generic.auto._
-import io.circe.generic.semiauto._
 
 object TwitterStreamer extends App {
 
-	val conf = ConfigFactory.load()
+  val conf = ConfigFactory.load()
 
-	//Get your credentials from https://apps.twitter.com and replace the values below
-	private val consumerKey = "cGKbjjB6ZUZc81TQJWMNtJoUd"
-	private val consumerSecret = "8H938MNsvMYHM79huL8l7rijNtSwczjLypubwzS8UKtp9KTtTL"
-	private val accessToken = "4447624812-5riaoBA6VGYXLUtJYSsIbnCGdsoVo3MwUi5aOHK"
-	private val accessTokenSecret = "42GTIaMCF5LqRJ7eDsrdyqutAlTELWJDcaxhfcxLiowKO"
-	private val url = "https://stream.twitter.com/1.1/statuses/sample.json"
+  //Get your credentials from https://apps.twitter.com and replace the values below
+  private val consumerKey = "cGKbjjB6ZUZc81TQJWMNtJoUd"
+  private val consumerSecret = "8H938MNsvMYHM79huL8l7rijNtSwczjLypubwzS8UKtp9KTtTL"
+  private val accessToken = "4447624812-5riaoBA6VGYXLUtJYSsIbnCGdsoVo3MwUi5aOHK"
+  private val accessTokenSecret = "42GTIaMCF5LqRJ7eDsrdyqutAlTELWJDcaxhfcxLiowKO"
+  private val newline = ByteString("\r\n")
 
-	implicit val system = ActorSystem()
-	implicit val materializer = ActorMaterializer()
+  private val url = "https://stream.twitter.com/1.1/statuses/sample.json"
+  private implicit val system = ActorSystem()
+  private implicit val materializer = ActorMaterializer()
 
-	private val consumer = new DefaultConsumerService(system.dispatcher)
+  private implicit val TweetDecoder: Decoder[Tweet] = deriveDecoder[Tweet]
 
-  implicit val TweetDecoder: Decoder[Tweet] = deriveDecoder[Tweet]
+  private val consumer = new DefaultConsumerService(system.dispatcher)
 
-	val source = Uri(url)
+  val source = Uri(url)
 
-	val headers = generateAuthHeader()
+  val headers = generateAuthHeader()
 
-	val httpRequest: HttpRequest = HttpRequest(
-		method = HttpMethods.GET,
-		uri = source,
-		headers = headers
-	)
+  val httpRequest: HttpRequest = HttpRequest(
+    method = HttpMethods.GET,
+    uri = source,
+    headers = headers
+  )
 
-	val connectionFlow = Http().outgoingConnectionHttps(source.authority.host.address, 443)
+  val connectionFlow = Http().outgoingConnectionHttps(source.authority.host.address, 443)
 
-  val newline = ByteString("\r\n")
+  val totalEvents = new AtomicLong(0)
+  val totalTweets = new AtomicLong(0)
 
-	val x = Source.single(httpRequest)
-				.via(connectionFlow)
-				.flatMapConcat {
-					case response if response.status.isSuccess() =>
-						response.entity.withoutSizeLimit().dataBytes.async
-					case other =>
-						println(s"Failed! $other")
-						Source.empty
-				}.async
-         .via(Framing.delimiter(newline, Int.MaxValue).async)
-         .via(decode[Tweet].async)
-         .to(Sink.foreach(x => println(x.id_str)))//.onComplete(_ => system.terminate)
+  val start = System.nanoTime()
 
-  x.run()
-  // like de.knutwalker.akka.stream.support.CirceStreamSupport.decode[T]
-  // but ignore unparsable tweets.
-  def decode[A: Decoder]: Flow[ByteString, A, NotUsed] = {
-    import io.circe.jawn.CirceSupportParser._
+  val msgFlow = Source.single(httpRequest)
+    .via(connectionFlow)
+    .flatMapConcat {
+      case response if response.status.isSuccess() =>
+        response.entity.withoutSizeLimit().dataBytes.async
+      case other =>
+        println(s"Failed! $other")
+        Source.empty
+    }.via(Framing.delimiter(newline, Int.MaxValue).async)
+     .via(passThruInjection(() => totalEvents.incrementAndGet()).async)
 
-    JsonStreamParser.flow[Json].filter(jsonHasFields("created_at", "text", "id_str")(_))
-      .map(json => decodeJson[A](json))
-      .collect {
-        case Xor.Right(e) => e
-      }
+  //msgFlow.runForeach(_ => totalEvents.incrementAndGet())
+
+  val classifyJsonFlow = msgFlow.via(JsonProcessing.parallelJsonClassifier(4).async)
+
+  val x = classifyJsonFlow
+    .via(JsonProcessing.parallelTweetDecoder(4).async)
+    .take(1000)
+    .runForeach(_ => totalTweets.incrementAndGet())
+
+  system.scheduler.schedule(Duration.apply(10, TimeUnit.SECONDS), Duration.apply(10, TimeUnit.SECONDS)) { () =>
+    println(s"Total events processed ${totalEvents.get}")
+    println(s"Total tweets processed ${totalTweets.get}")
   }
 
-  def decodeJson[A](json: Json)(implicit decoder: Decoder[A]) = {
-    decoder.decodeJson(json)
+  x.onComplete { done =>
+    val end = System.nanoTime()
+    println(s"Took: ${Duration.fromNanos(end - start).toSeconds} seconds")
+    println(s"Total events processed ${totalEvents.get}")
+
+    system.terminate()
   }
 
-  def jsonHasFields(fields: String*)(json: Json): Boolean = {
-    val obj = json.cursor.focus.asObject
-    obj.exists(current => current.size >= fields.size && fields.forall(current(_).isDefined))
+  def generateAuthHeader() = {
+    val oauthHeader: Future[String] = consumer.createOauthenticatedRequest(
+      KoauthRequest(
+        method = "GET",
+        url = url,
+        authorizationHeader = None,
+        body = None
+      ),
+      consumerKey,
+      consumerSecret,
+      accessToken,
+      accessTokenSecret
+    ).map(_.header)
+
+    // The program is isn't useful without this...
+    val authorizationHeader = Await.result(oauthHeader, Duration(5, TimeUnit.SECONDS))
+
+    List(HttpHeader.parse("Authorization", authorizationHeader) match {
+      case ParsingResult.Ok(h, _) => Some(h)
+      case _ => None
+    }).flatten
   }
 
-  def jsonHasAtleastOneField(fields: String*)(json: Json): Boolean = {
-    val cursor = json.cursor
-    fields.exists(cursor.downField(_).isDefined)
+  def passThruInjection[A](fn: () => Unit): Flow[A,A,NotUsed] = {
+    Flow[A].map { passThru =>
+      fn()
+      passThru
+    }
   }
 
-	def generateAuthHeader() = {
-		val oauthHeader: Future[String] = consumer.createOauthenticatedRequest(
-			KoauthRequest(
-				method = "GET",
-				url = url,
-				authorizationHeader = None,
-				body = None
-			),
-			consumerKey,
-			consumerSecret,
-			accessToken,
-			accessTokenSecret
-		).map(_.header)
-
-		// The program is isn't useful without this...
-		val authorizationHeader = Await.result(oauthHeader, Duration(5, TimeUnit.SECONDS))
-
-		List(HttpHeader.parse("Authorization", authorizationHeader) match {
-			case ParsingResult.Ok(h, _) => Some(h)
-			case _ => None
-		}).flatten
-	}
 }
